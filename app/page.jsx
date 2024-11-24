b08633e87d8ff8ec0f5bff92f9b91b55f60c0136
sedthanun <62070212@it.kmitl.ac.th>
2024-11-24 16:25:50 +0700

    new

--- app/page.jsx
+++ app/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import RegisterForm from '../components/RegisterForm'
 import { getUserFromCookie } from '../lib/getUser'
 import Dashboard from "../components/Dashboard"
-//commet2
+//commet21
 
 export default async function page() {
   const user = await getUserFromCookie()
